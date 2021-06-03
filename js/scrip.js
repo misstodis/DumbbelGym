64b67947a08b858cf7ecd6fbeda5ff256b28f7f8
misstodis <85105705+misstodis@users.noreply.github.com>
2021-06-03 12:19:59 +0200

    Update scrip.js

--- js/scrip.js
+++ js/scrip.js
@@ -1,1 +1,13 @@
-
+//  cursus video display
+function toggle(){
+    var VideoCursus= document.querySelector(".VideoCursus")
+    VideoCursus.classList.toggle("DiplayVideo")
+    var Video= document.querySelector("video")
+    Video.pause();
+    Video.currentTime = 0;
+  }
+  function playVideo(file)
+  {
+    var myVideo = document.getElementById("myVideo")
+    myVideo.src = file;
+  }
