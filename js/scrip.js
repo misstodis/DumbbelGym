f2e6b9e244ffc38ca8c61178c1d44b64755e3fa1
misstodis <85105705+misstodis@users.noreply.github.com>
2021-06-15 21:23:31 +0200

    Add files via upload

--- js/scrip.js
+++ js/scrip.js
@@ -1,3 +1,4 @@
+
 //  cursus video display
 function toggle(){
     var VideoCursus= document.querySelector(".VideoCursus")
@@ -11,3 +12,23 @@ function toggle(){
     var myVideo = document.getElementById("myVideo")
     myVideo.src = file;
   }
+  
+  //product detail
+var productImg = document.getElementById("product-detail-image");
+var smallImg = document.getElementsByClassName("small-product-image");
+smallImg[0].onclick = function()
+{
+  productImg.src =smallImg[0].src;
+}
+smallImg[1].onclick = function()
+{
+  productImg.src =smallImg[1].src;
+}
+smallImg[2].onclick = function()
+{
+  productImg.src =smallImg[2].src;
+}
+smallImg[3].onclick = function()
+{
+  productImg.src =smallImg[3].src;
+}
