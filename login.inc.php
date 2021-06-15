82c36b17f6534cdff383a7b81a2ca1835b2c60c8
Ahmad82450 <75832271+Ahmad82450@users.noreply.github.com>
2021-06-15 21:33:42 +0200

    Add files via upload

---
+++ login.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+if (isset($_POST["submit"]))
+{
+    $username = $_POST["username"];
+    $pwd = $_POST["password"];
+
+    require_once "funtion.php";
+    $conn = dbconnect();
+
+    if (emptyInputLogin($username, $pwd) !== false) {
+        header('Location: ../login.php?error=emptyinput');
+        exit();
+    }
+
+    loginUser($conn, $username, $pwd);
+}
+else {
+    header('Location: ../login.php');
+    exit();
+}
